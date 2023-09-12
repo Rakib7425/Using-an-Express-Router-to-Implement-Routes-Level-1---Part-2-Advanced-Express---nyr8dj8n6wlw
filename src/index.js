3ac2fc6d89824e79b93e81789ca3c0712ea8e4d0
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-12 23:05:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const routes = require("./app");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Router Middlewares
+app.use(express.json());
+app.use(routes);
 
 dotenv.config();
 
-app.listen(3000, () => console.log('Server running......'));
+app.listen(3000, () => console.log("Server running......"));
