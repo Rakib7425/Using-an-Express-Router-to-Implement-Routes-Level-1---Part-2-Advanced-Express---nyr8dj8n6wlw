67ae0962b97abf6c69c9e023512e96368516602d
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-13 22:47:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-const express = require("express");
-const app = express();
-
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const productRoute = require("./app");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-//Router Middlewares
-app.use(express.json());
-app.use("/api/v1/product", productRoute);
+const app = require('./app');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.listen(3000, () => console.log("https://expert-dollop-wqq65gg94v7c94w-3000.app.github.dev/", "OR  3000"));
+app.listen(3000, () => console.log('Server running......'));
