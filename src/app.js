92f5438af84d6658784071d9cd9168dfdc317e6a
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-13 22:47:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,81 +1,74 @@
-const fs = require("fs");
-const express = require("express");
-const router = express.Router();
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const router = new express.Router();
+
+//middleware
+router.use(express.json());
 
 //Including product.json file
-const product = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/product.json`));
-
+const product = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/product.json`)
+);
 // Defining The Router
 // Get all the products
-router.get("/", (req, res) => {
-	try {
-		//Write your code here
-		res.status(200).send({
-			status: "success",
-			results: product.length,
-			data: {
-				product,
-			},
-		});
-	} catch (error) {
-		res.status(400).json(error);
-	}
+router.get('/api/v1/product', (req, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      results: product.length,
+      data: {
+        product: product,
+      },
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+//Creating new Product
+router.post('/api/v1/product', (req, res) => {
+  try {
+    const { title, price } = req.body;
+    if (!title || !price) {
+      // If either title or price is missing, send a single error response
+      return res.status(404).json({
+        message: 'Title and price are required',
+        status: 'Error',
+      });
+    }
+    const newId = product[product.length - 1].id + 1;
+    const newProduct = { id: newId, title, price };
+    product.push(newProduct);
+    fs.writeFile(
+      `${__dirname}/../dev-data/product.json`,
+      JSON.stringify(product),
+      (err) => {
+        // If there is an error writing the file, send an error response
+        if (err) {
+          return res.status(500).json({
+            message: 'Error creating product',
+            status: 'Error',
+          });
+        }
+        // Otherwise, send the success response
+        res.status(201).json({
+          status: 'Success',
+          data: {
+            product: newProduct,
+          },
+        });
+      }
+    );
+  } catch (error) {
+    // If there is an unhandled error, send an error response
+    res.status(400).json({
+      message: 'Error creating product',
+      status: 'Error',
+    });
+  }
 });
 
-async function saveDataToDatabase(data) {
-	return new Promise((resolve, reject) => {
-		const jsonData = JSON.stringify(data);
+//Registering our Router
+app.use(router);
 
-		fs.writeFile(`${__dirname}/../dev-data/product.json`, jsonData, (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
-}
-
-//Create a new Product
-router.post("/", (req, res) => {
-	try {
-		//Write your code here
-		let newId = product[product.length - 1].id + 1;
-
-		let { title, price } = req.body;
-		// console.log(newId, lastProduct);
-
-		const newProduct = {
-			id: newId,
-			title,
-			price,
-		};
-
-		if (!title || !price) {
-			res.status(404).send({
-				message: "Title and price are required",
-				status: "Error",
-			});
-		} else if (price && title) {
-			product.push(newProduct);
-			saveDataToDatabase(product);
-
-			res.status(200).send({
-				status: "success",
-				data: {
-					product: newProduct,
-				},
-			});
-		}
-	} catch (error) {
-		res.status(400).send({
-			message: "Error creating product",
-			status: "Error",
-		});
-	}
-});
-
-
-
-
-module.exports = router;
+module.exports = app;
